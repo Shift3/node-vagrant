f64aa43a074175cc53e0664237b4b1b7f0f7d32b
Greg Goforth <greg.goforth@gmail.com>
2016-09-28 21:03:59 -0700

    express!

---
+++ app/routes/login.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var express = require('express'),
+  router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.render('login');
+});
+
+router.post('/', (req, res, next) => {
+  var users = require('../users.json');
+
+  for (var i = 0; i < users.users.length; i++) {
+    let user = users.users[i];
+    
+    if (user.email === req.body.email && user.password === req.body.password) {
+      //we found the user!
+      return res.redirect('/');
+    }
+  }
+});
+
+module.exports = router;
