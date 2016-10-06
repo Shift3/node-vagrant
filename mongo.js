706321120ba20b9d08521a31dcdf822f6078682e
Greg Goforth <greg.goforth@gmail.com>
2016-10-05 20:31:31 -0700

    deleteing mongo models

--- mongo.js
+++ mongo.js
@@ -19,17 +19,51 @@ let WidgetSchema = new mongoose.Schema({
 
 let Widget = mongoose.model('Widget', WidgetSchema);
 
-let widget = new Widget({
-  prongs: '22abc',
-  name: "Widget D",
-  colors: ['red', 'white', 'blue']
-});
+// let widget = new Widget({
+//   prongs: 22,
+//   name: "Widget E",
+//   colors: ['red', 'white', 'blue']
+// });
+//
+// widget.save()
+//   .then(() => {
+//     console.log(widget); 
+//   })
+//   .catch(err => {
+//     console.log('THE ERROR: ', err); 
+//   });
 
-widget.save()
-  .then(() => {
-    console.log(widget); 
-  })
-  .catch(err => {
-    console.log('THE ERROR: ', err); 
-  });
+// Widget.findById('57f5c02d9aa3fb291c55b8be')
+//   .then(widget => {
+//     widget.prongs = 'a200';
+//     widget.save()
+//       .then(() => {
+//         console.log('The updated widget', widget); 
+//       })
+//       .catch(err => {
+//         console.log(err); 
+//       });
+//   });
+
+function remove(id) {
+  return Widget.findByIdAndRemove(id);
+}
+
+function findAllAndLogLength() {
+  return Widget
+    .find({})
+    .then(widgets => console.log(widgets.length));
+}
+
+remove('57f5b699de7f07280246bbe9')
+  .then(findAllAndLogLength);
+
+// Widget.findByIdAndRemove('57f5b4458fe3d027637b2394')
+//   .then(() => {
+//     Widget.find({})
+//       .then(widgets => {
+//         console.log(widgets.length); 
+//       });
+//   });
+
 
