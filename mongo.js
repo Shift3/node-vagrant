8cd0a7beb6bd7f441619a2c30635e5b09f081d8c
Greg Goforth <greg.goforth@gmail.com>
2016-10-10 18:52:13 -0700

    mongo populates

--- mongo.js
+++ mongo.js
@@ -7,17 +7,47 @@ let mongoose = require('mongoose');
  */
 mongoose.connect('mongodb://greg:1234@ds139655.mlab.com:39655/nodegeekwise');
 let db = mongoose.connection;
+
 db.on('open', () => console.log('database connected'));
 mongoose.Promise = require('bluebird');
 
-let WidgetSchema = new mongoose.Schema({
-  prongs: {type: Number, required: true},
-  name: String,
-  created_at: {type: Date, default: Date.now},
-  colors: [String]
-});
+// let WidgetSchema = new mongoose.Schema({
+//   prongs: {type: Number, required: true},
+//   name: String,
+//   created_at: {type: Date, default: Date.now},
+//   colors: [String]
+// });
 
-let Widget = mongoose.model('Widget', WidgetSchema);
+// let UserSchema = new mongoose.Schema({
+//   firstName: String,
+//   lastName: String,
+//   email: {
+//     type: String,
+//     validate: {
+//       validator: function (value) {
+//         return /@/.test(value);
+//       },
+//       message: '{VALUE} is not a valid email'
+//     }
+//   }
+// });
+//
+// UserSchema.methods.sayHello = function () {
+//   return `Hello, I am ${this.firstName} ${this.lastName}`;
+// };
+//
+// let User = mongoose.model('User', UserSchema);
+//
+var user = new User({email: 'foo@bar.com', firstName: 'Greg', lastName: 'Goforth'});
+
+user.save()
+  .then(() => {
+    console.log(user);
+    console.log(user.sayHello());
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 // let widget = new Widget({
 //   prongs: 22,
@@ -45,18 +75,18 @@ let Widget = mongoose.model('Widget', WidgetSchema);
 //       });
 //   });
 
-function remove(id) {
-  return Widget.findByIdAndRemove(id);
-}
+// function remove(id) {
+//   return Widget.findByIdAndRemove(id);
+// }
 
-function findAllAndLogLength() {
-  return Widget
-    .find({})
-    .then(widgets => console.log(widgets.length));
-}
+// function findAllAndLogLength() {
+//   return Widget
+//     .find({})
+//     .then(widgets => console.log(widgets.length));
+// }
 
-remove('57f5b699de7f07280246bbe9')
-  .then(findAllAndLogLength);
+// remove('57f5b699de7f07280246bbe9')
+//   .then(findAllAndLogLength);
 
 // Widget.findByIdAndRemove('57f5b4458fe3d027637b2394')
 //   .then(() => {
